b0dcf9b079c53ef01af28135835f18ce22f8fca6
huyto <huyto@HUYTOAN>
2025-10-21 10:58:08 +0700

    update camera

--- Views/CCCD/CCCDPage.xaml.cs
+++ Views/CCCD/CCCDPage.xaml.cs
@@ -28,37 +28,6 @@ public partial class CCCDPage : ContentPage
         CccdFrame.HeightRequest = frameHeight;
     }
 
-    protected override void OnAppearing()
-    {
-        base.OnAppearing();
-        Dispatcher.Dispatch(async () =>
-        {
-            try
-            {
-                var cameras = await CameraPreview.GetAvailableCameras(CancellationToken.None);
-                var rearCamera = cameras.FirstOrDefault(c => c.Position == CameraPosition.Rear);
-
-                if (rearCamera != null)
-                {
-                    CameraPreview.SelectedCamera = rearCamera;
-                    if (CameraPreview.IsVisible)
-                    {
-                        await CameraPreview.StartCameraPreview(CancellationToken.None);
-                        Console.WriteLine(" Camera started successfully.");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine(" No rear camera found.");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($" Camera start error: {ex.Message}");
-            }
-        });
-    }
-
     protected override void OnDisappearing()
     {
         base.OnDisappearing();
@@ -84,6 +53,32 @@ public partial class CCCDPage : ContentPage
 
     private async void CameraPreview_Loaded(object sender, EventArgs e)
     {
-        
+        Dispatcher.Dispatch(async () =>
+        {
+            try
+            {
+                var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(3));
+                var cameras = await CameraPreview.GetAvailableCameras(cancellationTokenSource.Token);
+                var rearCamera = cameras.FirstOrDefault(c => c.Position == CameraPosition.Rear);
+
+                if (rearCamera != null)
+                {
+                    CameraPreview.SelectedCamera = rearCamera;
+                    if (CameraPreview.IsVisible)
+                    {
+                        await CameraPreview.StartCameraPreview(CancellationToken.None);
+                        Console.WriteLine(" Camera started successfully.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(" No rear camera found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($" Camera start error: {ex.Message}");
+            }
+        });
     }
 }
